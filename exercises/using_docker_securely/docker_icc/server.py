b15f289ee30e25b5721fb3a542077cbbe6508df0
Traiko Dinev <tdinev@skillerwhale.com>
2024-02-12 15:01:20 +0000

    [docker-security] Update exercises

--- exercises/using_docker_securely/docker_icc/server.py
+++ exercises/using_docker_securely/docker_icc/server.py
@@ -7,10 +7,10 @@ def index():
     return 'Hello'
 
 @app.route('/get_secret')
-def get_data():
+def get_secret():
     # Don't put secrets in source code!
     #   For exercise purposes only.
     return jsonify({'secret': 'supersecret'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=2001)
